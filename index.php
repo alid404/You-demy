a6e67be20a789f14ca4385ae9b74102660a7d2d6
alid404 <myketyson2279@hotmail.com>
2025-01-17 17:03:40 +0100

    create the index page for the user to be able to login or register

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Youdemy | Home</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0">
+    <link rel="stylesheet" href="./src/css/style.css">
+    <script src="./src/js/main.js" defer></script>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <span class="hamburger-btn material-symbols-rounded">menu</span>
+            <a href="/" class="logo">
+                <img src="./src/img/logo.jpg" alt="logo">
+                <h2>Youdemy</h2>
+            </a>
+            <ul class="links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Courses</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Contact us</a></li>
+            </ul>
+            <div style="display: flex; align-items: center; gap: 10px;">
+                <a href="./login/index.php"><button class="login-btn">LOG IN</button></a>
+                <a href="./register/index.php"><button class="login-btn">REGISTER</button></a>
+            </div>
+        </nav>
+    </header>
+
+    <div class="blur-bg-overlay"></div>
+    
+</body>
+</html>
